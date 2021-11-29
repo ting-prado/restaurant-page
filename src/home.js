2a41792369d615027a46967b63cd03cc84d1f0cb
Ethel <ethel.prado1998@gmail.com>
2021-11-29 15:23:54 +0800

    Contact page design layout on HTML

--- src/home.js
+++ src/home.js
@@ -11,7 +11,7 @@ const loadHome = () => {
     homelogo.alt = 'yukihira diner homepage logo';
     const description = document.createElement('p');
     description.id = 'desc';
-    description.textContent = 'Yukihira pushes the envelope of japanese cuisine. Taking its influences from our team members\'s culinary roots, Yukihira blends traditional and innovative techniques to create unique offerings using local ingredients in all of its dishes. Yukihira is grounded in hospitality with our staff\'s commitment to providing you with a memorable experience each time you walk through our door.';
+    description.textContent = 'Yukihira pushes the envelope of Japanese cuisine. Taking its influences from our team members\' culinary roots, this diner blends traditional and innovative techniques to create unique offerings using local ingredients in all of its dishes. We are grounded in hospitality with our staff\'s commitment to provide you with a memorable experience every time you walk through our door.';
     const foodImg = new Image();
     foodImg.src = food;
     foodImg.id = 'food';
